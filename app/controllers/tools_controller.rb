e5c4ff6b4b6efd6927a29f1f00140576fe2e637c
Marie Chauvois <mrchvsghn@gmail.com>
2020-11-17 14:05:16 +0100

    new and show pages for tools done

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,19 +1,23 @@
 class ToolsController < ApplicationController
 
+
   def index
     @tools = policy_scope(Tool).order(created_at: :desc)
   end
 
   def show
     @tool = Tool.find(params[:id])
+    authorize @tool
   end
 
   def new
     @tool = Tool.new
+    authorize @tool
   end
 
   def create
     @tool = Tool.new(tool_params)
+    @tool.user = current_user
     authorize @tool
     if @tool.save
       redirect_to @tool, notice: "Tool was successfully added"
@@ -24,7 +28,11 @@ class ToolsController < ApplicationController
 
   private
 
+  def find_user
+    @user = User.find(params[:id])
+  end
+
   def tool_params
-    params.require(:tool).permit(:name, :category, :description)
+    params.require(:tool).permit(:name, :category, :description, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'tools/new'
-  get 'tools/create'
-  get 'tools/index'
-  get 'tools/show'
+
   devise_for :users
   root to: 'pages#home'
+  resources :tools
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
