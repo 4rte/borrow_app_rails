3a2fb233cc468df9b181ec64f56ce5874d79fa3a
Anna Ptasinska <itsme@hellothisisanna.com>
2020-11-17 15:19:15 +0100

    homepage-front-end-done with font change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,7 +28,16 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your functions here, e.g:
+//   // initSelect2();
+// });
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
 });
+
+console.log("Hello from app/javascript/packs/application.js!");
