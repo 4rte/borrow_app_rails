aaa937f8f42906e3fae448836d0337fdf648ab42
Marie Chauvois <mrchvsghn@gmail.com>
2020-11-22 23:07:27 +0100

    Revert "Petly style"
    This reverts commit 4380bb75f62706f65b5f4923a9c86a7276c3858f.

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,12 +3,3 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-
-import Typed from 'typed.js';
-
-var options = {
-  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  typeSpeed: 40
-};
-
-var typed = new Typed('.element', options);
